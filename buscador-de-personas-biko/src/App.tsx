e8726393181f7b0a52e5c8630c9b9756d144377b
KevinTomasKlein <kevin.tomas@biko2.com>
2021-04-26 11:44:15 +0200

    Kevin:Instalación de dependencias fontAwasome, actualización de npm, creación de hoja de estilos de la vista HomeView, implementación de estilos e icono de búsqueda

--- buscador-de-personas-biko/src/App.tsx
+++ buscador-de-personas-biko/src/App.tsx
@@ -3,20 +3,16 @@ import logo from "./logo.svg";
 import logoBiko from "./logoBiko.svg";
 import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
-import { HomeView } from "./views/HomeView";
+import HomeView from "./views/HomeView";
+import "./styles/layoutHomeView.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="navbar">
+    <div className="">
+      <header>
         <picture>
           <source srcSet={logoBiko} type="image/svg+xml" />
-          <img
-            src={logoBiko}
-            className="img-thumbnail rounded float-start"
-            alt="logo"
-            style={{ width: 200, height: 200 }}
-          />
+          <img src={logoBiko} className="logo" alt="logo" />
         </picture>
       </header>
       <body>
--- buscador-de-personas-biko/src/views/HomeView.tsx
+++ buscador-de-personas-biko/src/views/HomeView.tsx
@@ -1,66 +1,71 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-export function HomeView() {
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+function HomeView() {
   const [datosBiko, setDatosBiko] = useState<any[]>([]);
   const [error, setError] = useState(null);
-  const getBikoEmployeesData = async () => {
-    await axios
-      .get("./data.json")
-      .then((response) => {
-        setDatosBiko(response.data);
-      })
-      .catch((error) => {
-        setError(error);
-      });
-  };
 
   useEffect(() => {
-    getBikoEmployeesData();
+    const fetchData = async () => {
+      const { data } = await axios.get("dataBiko.json");
+      setDatosBiko(data);
+    };
+    fetchData();
   }, []);
 
-  return (
-    <div className="container-fluid">
-      <div>
-        <div className="navbar navbar-light bg-light">
-          <div className="container-fluid">
-            <form className="d-flex offset-lg-10 offset-md-8">
-              {/* <input
-                  className="form-control me-2"
-                  type="text"
-                  placeholder="Search Flower"
-                  aria-label="Search Flower"
-                  onChange={(event) => getSearchedData(event)}
-                ></input> */}
-            </form>
+  function printAllBikoEmployes() {
+    let Result: any = datosBiko.map((employesData) => {
+      return (
+        <div className="col-md-4 col-lg-3">
+          <div className="card">
+            <img
+              alt="employee-image"
+              className="card-image mx-auto"
+              style={{ width: "200px", height: "200x" }}
+              src={employesData["ImgUrl"]}
+            ></img>
+            <div className="card-body">
+              <h5 className="card-title">
+                {"Nombre: " + employesData["Nombre"]}
+              </h5>
+              <h5 className="card-title">
+                {"Apellido: " + employesData["Apellidos"]}
+              </h5>
+              <p className="card-text">{"Equipo: " + employesData["Equipo"]}</p>
+              <p className="card-text">{"Rol: " + employesData["Rol"]}</p>
+            </div>
           </div>
         </div>
-        <div className="row">
-          {datosBiko.map((employesData) => (
-            <div className="col-3 ">
-              <div className="card">
-                <img
-                  alt="flower-images"
-                  className="card-image"
-                  style={{ width: 200, height: 200 }}
-                  src="../images/mock-up-image.jpg"
-                ></img>
-                <div className="card-body">
-                  <h5 className="card-title">{employesData.Nombre}</h5>
-                  <p className="card-text">
-                    {"Binomial name: " + employesData.Apellidos}
-                  </p>
-                  <p className="card-text">
-                    {"Precio: " + employesData.Equipo + " €"}
-                  </p>
-                  <p className="card-text">
-                    {"Waterings per week: " + employesData.Rol}
-                  </p>
-                </div>
-              </div>
-            </div>
-          ))}
+      );
+    });
+    return Result;
+  }
+
+  return (
+    <div className="container">
+      <h1 className="header-title">Busca Bikonianos</h1>
+      <p className="red-text">(lorem ipsum dolor set)</p>
+      <p className="text-above-header">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vitae
+        pretium tellus.
+      </p>
+      <div className="input-group">
+        <input
+          type="text"
+          placeholder="Nombre bikoniano"
+          className="form-control"
+          onChange={(event) => event.target.value}
+        ></input>
+        <div className="input-group-append ">
+          <button className="icon-search btn btn-primary">
+            <FontAwesomeIcon icon={faSearch} />
+          </button>
         </div>
       </div>
+      <div className="row">{printAllBikoEmployes()}</div>
     </div>
   );
 }
+export default HomeView;
