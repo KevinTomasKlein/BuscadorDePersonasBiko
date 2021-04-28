07809bd3af5175cc6d5d6284324ab46d8ea28e66
KevinTomasKlein <kevin.tomas@biko2.com>
2021-04-28 11:26:48 +0200

    Kevin:Cambio de logo svg a png en App.tsx, cambios en estilos de página detalles, y adición de version sin terminar de los detalles de un trabajador

--- buscador-de-personas-biko/src/views/DetailsView.tsx
+++ buscador-de-personas-biko/src/views/DetailsView.tsx
@@ -1,30 +1,50 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faRocket } from "@fortawesome/free-solid-svg-icons";
+import { faCalendar } from "@fortawesome/free-solid-svg-icons";
+
 export function DetailsView({ location }: any) {
   const employeDetails = location.state.employeData;
-
+  const AllEmployes: any[] = location.state.AllEmployes;
   return (
-    <div className="container-fluid ">
-      <div className="row">
-        <div className="card-horizontal ">
+    <div className=" displayContainer">
+      <div className="container-fluid">
+        <div className="flex-row d-flex col-md-10 ">
           <img
             alt="employe-image"
-            className="card-image mx-auto"
-            style={{ width: "729.57px", height: "711.28px" }}
+            className="float-left img-fluid"
             src={employeDetails["ImgUrl"]}
           ></img>
           <div className="card-body text-left">
-            <p className="card-title text">{employeDetails["Nombre"]}</p>
-            <h5 className="card-title bold-text">
+            <p className="card-title title-text">{employeDetails["Nombre"]}</p>
+            <h5 className="card-title bold-title-text">
               {employeDetails["Apellidos"]}
             </h5>
             <p className="card-text red-text">
               {"(" + employeDetails["Equipo"] + ")"}
             </p>
+            <div>
+              <div className="d-inline-flex">
+                <FontAwesomeIcon icon={faRocket} />
+                <p className=" bold-text">{employeDetails["Equipo"]}</p>
+              </div>
+
+              <div className="d-inline-flex ">
+                <FontAwesomeIcon icon={faCalendar} />
+                <p className=" date-text ml-2 mr-1">Desde </p>
+                <p className="mx-0 bold-text">
+                  {employeDetails["Fecha incorporación a Biko"].split("/")[2]}
+                </p>
+              </div>
+            </div>
             <p className="card-text">{"Rol: " + employeDetails["Rol"]}</p>
           </div>
+        </div>
+        <div className="row">
+          <h5 className="bold-title-text">Otra gente de fenix</h5>
         </div>
       </div>
     </div>
