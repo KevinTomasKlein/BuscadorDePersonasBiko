0f58a6d541fd6bd04c5075590c4ed180ba3a9630
KevinTomasKlein <kevin.tomas@biko2.com>
2021-05-11 14:42:56 +0200

    Kevin:Creación de servicio para recoger datos de Json

--- buscador-de-personas-biko/src/services/getBikoData.tsx
+++ buscador-de-personas-biko/src/services/getBikoData.tsx
@@ -1,1 +1,34 @@
-export default function getBikoData() {}
+import axios from "axios";
+
+export default function getEmployes() {
+  const fetchData = async () => {
+    const { data } = await axios.get("dataBiko.json");
+    const employes = data.map((employe: any) => {
+      const {
+        ImgUrl,
+        Nombre,
+        Apellidos,
+        Rol,
+        FechaIncorporacion,
+        SobreMi,
+        Equipo,
+        Tecnologias,
+        Id,
+      }: any = employe;
+
+      return {
+        Nombre,
+        Apellidos,
+        Rol,
+        ImgUrl,
+        FechaIncorporacion,
+        SobreMi,
+        Equipo,
+        Tecnologias,
+        Id,
+      };
+    });
+    return employes;
+  };
+  return fetchData();
+}
