10122acb12e4e6806865015d2895c3df057ef48b
KevinTomasKlein <kevin.tomas@biko2.com>
2021-05-11 14:40:50 +0200

    Kevin: Creacion de los componentes Employe, ListOfEmploye, ListOfMembers

--- buscador-de-personas-biko/src/components/Employe.tsx
+++ buscador-de-personas-biko/src/components/Employe.tsx
@@ -1,1 +1,21 @@
-export default function Employe() {}
+export default function Employe({ ImgUrl, Nombre, Apellidos, Rol }: any) {
+  return (
+    <>
+      <div className="card team-picture">
+        <img
+          alt="employe"
+          className="card-image mx-auto"
+          style={{ width: "264px", height: "264px" }}
+          src={ImgUrl}
+        ></img>
+        <div className="img_description_layer">
+          <div className="img_text_container img_employe_data">
+            <p className="monstserrat">{Nombre}</p>
+            <p className="monstserrat">{Apellidos}</p>
+            <p className="monstserrat-normal">{Rol}</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
--- buscador-de-personas-biko/src/components/ListOfMembers.tsx
+++ buscador-de-personas-biko/src/components/ListOfMembers.tsx
@@ -1,1 +1,22 @@
-export default function ListOfMembers() {}
+import { Link } from "react-router-dom";
+import EmployeCard from "./Employe";
+
+export default function ListOfMembers({ members, AllEmployes, url }: any) {
+  return members.map((member: any) => (
+    <div key={member.Id}>
+      <Link
+        to={{
+          pathname: `${url}/${member["Nombre"]} ${member.Apellidos}`,
+          state: { employeData: member, AllEmployes: AllEmployes },
+        }}
+      >
+        <EmployeCard
+          Nombre={member.Nombre}
+          Apellidos={member.Apellidos}
+          Rol={member.Rol}
+          ImgUrl={member.ImgUrl}
+        />
+      </Link>
+    </div>
+  ));
+}
